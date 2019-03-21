d6905cb9ac070c92ce0b11c95c78b1f47db8b185
Paul TRUFFAULT <ptruffau@e2r6p5.42.fr>
2019-03-21 13:01:41 +0100

    make save

--- srcs/exec/ft_execve.c
+++ srcs/exec/ft_execve.c
@@ -33,8 +33,6 @@ static int		ft_builtins(t_shell *sh, t_process *p, t_tree *t, int frk)
 			ft_exit_son(t, sh, p->ret);
 		return (1);
 	}
-	sh->env = ft_new_envv(sh->env, "_", p->cmd);
-	sh->intern = ft_new_envv(sh->intern, "_", p->cmd);
 	return (0);
 }
 

64229b37cbbbc0cd59ba6e1ef44b6b95961a432a
Alexandre DI ROSA <adi-rosa@e2r5p7.42.fr>
2019-04-03 16:33:18 +0200

    done

--- includes/shell42.h
+++ includes/shell42.h
@@ -34,13 +34,13 @@ void		set_son_signal(void);
 int			ft_init_groups(t_shell *sh);
 
 int			ft_fc(t_shell *sh, char **argv);
-int			search_in_hist_parser(t_fc *fc);
+int			search_in_hist_parser(t_fc *fc, short i);
 t_hist		*search_by_number_from_first(t_hist *first, int nb);
 t_hist		*search_by_number_from_last(t_hist *first, int nb);
 t_hist		*search_by_occurence(t_hist *first, char *to_search);
 void		ft_fc_option_l(t_fc *fc);
 void		ft_fc_option_e(t_fc *fc);
-void		ft_fc_option_s(t_fc *fc);
+void		ft_fc_option_s(t_fc *fc, int pos);
 
 void 		ft_setup_localenv(t_process *p, t_shell *sh, t_tree *t);
 void		ft_get_envv_back(t_shell *sh, t_process *p, t_tree *t);
--- srcs/builtins/ft_fc/ft_fc_option_s.c
+++ srcs/builtins/ft_fc/ft_fc_option_s.c
@@ -12,7 +12,92 @@
 
 #include "shell42.h"
 
-void ft_fc_option_s(t_fc *fc)
+char		*replace_occurances(char *old, char *new, char *str)
 {
-	(void)fc;
+	size_t	x;
+	char	*tmp;
+	size_t	old_size;
+	size_t	new_size;
+
+	new_size = ft_strlen(new);
+	old_size = ft_strlen(old);
+	tmp = NULL;
+	x = 0;
+	while (str[x])
+	{
+		if (ft_str_startwith(str + x, old))
+		{
+			if (!(tmp = ft_strpull(str, str + x, (int)old_size - 1, new)))
+				return (NULL);
+			ft_strdel(&str);
+			str = tmp;
+			x += new_size;
+		}
+		else
+			++x;
+	}
+	return (str);
+}
+
+static t_hist	*ft_fc_option_s_replace(t_fc *fc, int pos)
+{
+	char	**tabl;
+	t_hist	*hist;
+	char	*tmp;
+
+	if (!(tabl = ft_strsplit(fc->av[pos], '=')))
+	{
+		error("Memory allocation failed", NULL);
+		return (NULL);
+	}
+	if (!tabl[0] || (tabl[0] && !tabl[1]))
+	{
+		error("fc -s [OLD=NEW] [FIRST]", NULL);
+		return (NULL);
+	}
+	if (fc->av[pos + 1])
+	{
+		if ((fc->av[pos + 1][0] >= '0' && fc->av[pos + 1][0] <= '9') || fc->av[pos + 1][0] == '-')
+			fc->first = ft_atoi(fc->av[pos + 1]) == 0 ? -1 : ft_atoi(fc->av[pos + 1]);
+		else
+			fc->first_ = fc->av[pos + 1];
+	}
+	if (search_in_hist_parser(fc, 1) == FAILURE)
+		return (NULL);
+	hist = fc->shell->hist;
+	if (!(tmp = replace_occurances(tabl[0], tabl[1], ft_strdup(fc->hist_first->s))))
+		return (NULL);
+	ft_strdel(&hist->s);
+	hist->s = tmp;
+	return (hist);
+}
+
+void	ft_fc_option_s(t_fc *fc, int pos)
+{
+	t_tree	*t;
+	t_hist	*hist;
+
+	fc->first = 0;
+	hist = NULL;
+	if (fc->av[pos] && ft_strchr(fc->av[pos], '=') && !(hist = ft_fc_option_s_replace(fc, pos)))
+		return ;
+	else if (fc->av[pos] && hist == NULL)
+	{
+		if ((fc->av[pos][0] >= '0' && fc->av[pos][0] <= '9') || fc->av[pos][0] == '-')
+			fc->first = ft_atoi(fc->av[pos]) == 0 ? -1 : ft_atoi(fc->av[pos]);
+		else
+			fc->first_ = fc->av[pos];
+		if (search_in_hist_parser(fc, 1) == FAILURE)
+			return ;
+		hist = fc->shell->hist;
+		ft_strdel(&hist->s);
+		if (!(hist->s = ft_strdup(fc->hist_first->s)))
+		{
+			error("Memory allocation failed", NULL);
+			return ;
+		}
+		ft_printf("[%s]\n", hist->s);
+	}
+	if ((t = get_tree(hist->s)))
+		ft_free_tree(exec_tree(t, fc->shell));
 }

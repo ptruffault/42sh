19ce6422318db9326bb1d8768e621c191cf817c1
Fabien Stadelwieser <fabien.stadelwieser@epitech.eu>
2019-04-27 13:43:59 +0200

    open with a great fix

--- lib/libft/srcs/ft_open.c
+++ lib/libft/srcs/ft_open.c
@@ -6,7 +6,7 @@
 /*   By: ptruffau <ptruffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/13 15:28:15 by ptruffau          #+#    #+#             */
-/*   Updated: 2019/04/24 17:33:46 by fstadelw         ###   ########.fr       */
+/*   Updated: 2019/04/27 13:42:08 by fstadelw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,23 @@ int	ft_open(const char *path, int flag, mode_t mode)
 	struct stat	inf;
 
 	fd = -1;
-	if (!(flag & O_CREAT) && lstat(path, &inf) == -1)
-		error("no such file or directory", path);
-	else if (!(flag & O_CREAT) && S_ISDIR(inf.st_mode))
-		error("is a directory", path);
-	else if (!(flag & O_CREAT) && !(inf.st_mode & S_IRUSR))
-		error("permission denied", path);
-	else if (!(flag & O_CREAT) && S_ISLNK(inf.st_mode))
-		error("ft_open do not follow symbolic link", path);
-	else if (((fd = open(path, flag, mode)) < 0) && (S_ISFIFO(inf.st_mode)))
-		error("can't open this file", path);
+	if (lstat(path, &inf) == -1)
+	{
+		if (!(flag & O_CREAT))
+			error("no such file or directory", path);
+		else if ((fd = open(path, flag, mode)) < 0)
+			error("can't open this file", path);
+	}
+	else
+	{
+		if (S_ISDIR(inf.st_mode))
+			error("is a directory", path);
+		else if (!(flag & O_CREAT) && !(inf.st_mode & S_IRUSR))
+			error("permission denied", path);
+		else if (S_ISLNK(inf.st_mode))
+			error("ft_open do not follow symbolic link", path);
+		else if ((fd = open(path, flag, mode)) < 0)
+			error("can't open this file", path);
+	}
 	return (fd);
 }
